8551abedda0a2c1087e997f21fe81c2d9779dce0
Daniel Lopez <tauron32@gmail.com>
2012-05-30 01:53:47 +0200

    Basic layout with Bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the title" do
+    it "should have the h1 title" do
       visit '/static_pages/home'
-      page.should have_selector('h1', text: " | Home")
+      page.should have_selector('h1', :text => "1 centimo")
+    end
+    
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "1 centimo")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Inicio')
     end
   end
 end
