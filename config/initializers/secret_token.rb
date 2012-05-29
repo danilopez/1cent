b9e6f6ce7e2b6dbfcbc24e9a73bd77b9c0a7ab30
Daniel Lopez <tauron32@gmail.com>
2012-05-30 00:14:52 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Centimos::Application.config.secret_token = '7c25157e003811c01a4ec6889bec7fe33d1ebe0c48b4bc18fd91ee7e13d37ba1155a3f7f503519267f269bbc8e3d2ebb7a89fe0bf3d31f3f5290b3c837dcfe76'
